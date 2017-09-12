d02794daf0a239539a48329ae39a3b20a59e6db2
KshitijRajvanshi <rajvankj@mail.uc.edu>
2017-09-11 21:27:00 -0400

    Spring DI with Annotations demo with @Autowired and @Qualifiers

--- Spring Annotations DI demo/DatabaseService.java
+++ Spring Annotations DI demo/DatabaseService.java
@@ -3,12 +3,12 @@ package com.spring.demo.annotations;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FortuneServiceTeller implements FortuneService {
+public class DatabaseService implements FortuneService {
 
 	@Override
 	public String getFortune() {
 		// TODO Auto-generated method stub
-		return "You will win most of ur back-end volleys today";
+		return null;
 	}
 
 }
--- Spring Annotations DI demo/TennisCoach.java
+++ Spring Annotations DI demo/TennisCoach.java
@@ -1,18 +1,38 @@
 package com.spring.demo.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-
+	
+	//Field injection
+	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService)
+	
+	/* For constructor injection
+	 * @Autowired
+	 * public TennisCoach(@Qualifier("randomFortuneService")FortuneService theFortuneService)
 	{
 		fortuneService = theFortuneService;
+	}*/
+	
+	
+	
+	public TennisCoach()
+	{
+		System.out.println("Inside default constructor for TennisCoach");
 	}
+	
+	// For setter injection
+	/*@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
@@ -23,5 +43,7 @@ public class TennisCoach implements Coach {
 	public String getFortune() {
 		return fortuneService.getFortune();
 	}
+
+	
 
 }
